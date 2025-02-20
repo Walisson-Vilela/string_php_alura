0636b5e405fd5e3e8bf7992a413d95d6ca36ea5f
walissonmw <walisson@mundowap.com.br>
2025-02-20 11:31:06 -0300

    start project

---
+++ exec.sh
@@ -0,0 +1,1 @@
+docker exec -it project-app_php_project-app_1 bash
---
+++ public/index.php
@@ -0,0 +1,15 @@
+<?php
+
+$debug1 = 1;
+
+$debug2 = 2;
+
+$debug3 = 3;
+
+$debug4 = 4;
+
+$debug5 = 5;
+
+$debug6 = 6;
+
+phpinfo();
